6af641d40bfa9e15d73794629aa33f496a72ff1d
Joel Williams <joel.williams90@gmail.com>
2024-02-27 10:42:47 -0500

    python_notes

--- learning Python/if_statements.py
+++ learning Python/if_statements.py
@@ -1,4 +1,4 @@
-cars = ['audi', 'bmw', 'subaru', 'toyta']
+cars = ['audi', 'bmw', 'subaru', 'toyta', "Honda"]
 
 for car in cars:
     if car == 'bmw':
@@ -6,7 +6,72 @@ for car in cars:
     else:
         print(car.title())
         
-requested_topping = 'mushrooms'
+# requested_topping = 'mushrooms'
 
-if requested_topping != 'anchovies':
-    print("hold the anchovies!")
+# if requested_topping != 'anchovies':
+#     print("hold the anchovies!")
+    
+age0 = 22
+age1 = 18
+
+
+# if age0 > 20 and age1 < 20:
+#     print(False)
+
+car = "Honda"
+if car not in cars:
+    print(f"{car.title()}, you aren't here")
+else:
+    print(f"{car.title()}, whoa")
+    
+    print("Is car == 'Honda'? I predict True.")
+    print(car == "Honda")
+    
+    print("\nIs car == 'audi'? I predict False.")
+    print(car == 'audi')
+    
+age = 10
+if age > 9:
+    print("You are old enough")
+    print("Have you started 5th grade?")
+else: 
+    print("Too young!")
+    
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+
+print(f"Your admission costs ${price}")
+
+requested_toppings = ['mushrooms', 'extra cheese', 'pepperoni']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms")
+if "tomato" in requested_toppings:
+        print("Adding tomato.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese")
+    
+print("\nFinished making your pizza!")
+
+
+alien_color = 'green'
+if alien_color == 'red':
+    print('1 point earned')
+elif alien_color == 'yellow':
+    print('2 points earned')
+elif alien_color == 'green':
+    print('5 points earned!')
+else:
+    print(alien_color)
+    
+for requested_toppin in requested_toppings:
+    print(f"Adding {requested_toppin}.")
+    
+print("\nFinished making your pizza!")
---
+++ learning Python/intro_class.py
@@ -0,0 +1,19 @@
+# data type is a classification, mostly needed for a computer so that the computer can know how you intend to use the data in the future.
+
+# Data type is a container for data. Data structure
+# Python comes with built-in data types.
+# String - sequence of characters. Strings are immutable.
+# int - whole number
+# float - number with decimals
+# boolean - True or False. immutable
+# Dictionary - {'Key': 'Value'}. Apparently the most popular job interview question.'Can tuple be used as a key in a dictionary?'. 'Can you use list as a key in a dictionary?'. Only immutable data type can be used as a key in a dictionary. Int, float, string, boolean, tuple. Anything else can be used as a value.
+# list - sequence of values seperated by values, held in [] and has indicies
+# tuple - immutable data type held in (). data usually comes in form of tuple. if function returns more than 1 result, it's packaged as a tuple.
+# dictionary is fast because keys are unique. If you store phone numbers in a list then you need to iterate.
+# set - collection of immutable items. set itself is mutable. 
+# range - generates sequence of ints; start (inclusive) to stop (exclusive)
+# 1. type
+# 2. dir - helps you to see what is possible with current code. 
+# 3. help - function that does exactly what it says, 'help'.
+
+
